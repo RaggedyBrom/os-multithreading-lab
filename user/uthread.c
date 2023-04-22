8c47fc749490c1b16222971815a5c606c8fc62e7
Jeremy <brom.eeyhib@gmail.com>
2023-04-22 13:35:59 -0400

    Modify thread struct to store necessary registers

--- user/uthread.c
+++ user/uthread.c
@@ -14,6 +14,22 @@
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+
+  // Store the program counter, stack pointer, and all callee-saved registers
+  uint64     pc;
+  uint64     sp;
+  uint64     s0;
+  uint64     s1;
+  uint64     s2;
+  uint64     s3;
+  uint64     s4;
+  uint64     s5;
+  uint64     s6;
+  uint64     s7;
+  uint64     s8;
+  uint64     s9;
+  uint64     s10;
+  uint64     s11;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
